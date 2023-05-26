d5a0518f5010e61eccadc07363c77e4a016218ed
Evgeny E <www.sam.klg@yandex.ru>
2023-05-26 17:59:38 +0300

    right game

--- my-app/src/Board.js
+++ my-app/src/Board.js
@@ -28,21 +28,29 @@ const Board = ({ xIsNext, squares, onPlay }) => {
 	return (
 		<>
 			<div className="status">{status}</div>
-			<div className="board-row">
-				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-			</div>
-			<div className="board-row">
-				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-			</div>
-			<div className="board-row">
-				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-			</div>
+			{squares.map((value, index) => {
+				if (index === 0 || index === 3 || index === 6) {
+					return (
+						<div key={index} className="board-row">
+							<Square
+								key={index}
+								value={squares[index]}
+								onSquareClick={() => handleClick(index)}
+							/>
+							<Square
+								key={index + 1}
+								value={squares[index + 1]}
+								onSquareClick={() => handleClick(index + 1)}
+							/>
+							<Square
+								key={index + 2}
+								value={squares[index + 2]}
+								onSquareClick={() => handleClick(index + 2)}
+							/>
+						</div>
+					);
+				}
+			})}
 		</>
 	);
 };
