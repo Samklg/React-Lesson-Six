5237dac124594368e157651e901971bfc6e56efe
Evgeny E <www.sam.klg@yandex.ru>
2023-06-02 18:29:39 +0300

    form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,99 @@
 import React, { useState } from 'react';
-import Board from './Board';
-import calculateWinner from './calculateWinner';
+import './App.css';
 
-const App = () => {
-	const [currentSquares, setCurrentSquares] = useState(Array(9).fill(null));
-	const [xIsNext, setXIsNext] = useState(true);
+function App() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [repeatPassword, setRepeatPassword] = useState('');
+	const [emailError, setEmailError] = useState('');
+	const [passwordError, setPasswordError] = useState('');
+	const [repeatPasswordError, setRepeatPasswordError] = useState('');
+	const [formValid, setFormValid] = useState(false);
 
-	const handlePlay = (nextSquares) => {
-		setCurrentSquares(nextSquares);
-		setXIsNext(!xIsNext);
+	const validateEmail = (email) => {
+		if (!/\S+@\S+\.\S+/.test(email)) {
+			setEmailError('Некорректный email');
+		} else {
+			setEmailError('');
+		}
+		setEmail(email);
 	};
 
-	const handleReset = () => {
-		setCurrentSquares(Array(9).fill(null));
-		setXIsNext(true);
+	const validatePassword = (password) => {
+		if (password.length < 6) {
+			setPasswordError('Пароль должен содержать не менее 6 символов');
+		} else {
+			setPasswordError('');
+		}
+		setPassword(password);
 	};
 
-	const winner = calculateWinner(currentSquares);
-	let status;
-	if (winner) {
-		status = 'Победил: ' + winner;
-	} else {
-		status = 'Сейчас ходит: ' + (xIsNext ? 'X' : 'O');
-	}
+	const validateRepeatPassword = (repeatPassword) => {
+		if (repeatPassword !== password) {
+			setRepeatPasswordError('Пароли не совпадают');
+		} else {
+			setRepeatPasswordError('');
+		}
+		setRepeatPassword(repeatPassword);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log('Email:', email);
+		console.log('Password:', password);
+		console.log('Repeat Password:', repeatPassword);
+	};
+
+	const handleBlur = () => {
+		if (
+			emailError ||
+			passwordError ||
+			repeatPasswordError ||
+			!email ||
+			!password ||
+			!repeatPassword
+		) {
+			setFormValid(false);
+		} else {
+			setFormValid(true);
+		}
+	};
 
 	return (
-		<div className="game">
-			<div className="game-board">
-				<Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-			</div>
-			<div className="game-info">
-				<div className="status">{status}</div>
-				<button onClick={handleReset}>Начать заново</button>
-			</div>
+		<div className="App">
+			<form onSubmit={handleSubmit}>
+				<label>Email:</label>
+				<input
+					type="email"
+					value={email}
+					onChange={(e) => validateEmail(e.target.value)}
+					onBlur={() => handleBlur()}
+				/>
+				{emailError && <div className="error">{emailError}</div>}
+				<label>Пароль:</label>
+				<input
+					type="password"
+					value={password}
+					onChange={(e) => validatePassword(e.target.value)}
+					onBlur={() => handleBlur()}
+				/>
+				{passwordError && <div className="error">{passwordError}</div>}
+				<label>Повторите пароль:</label>
+				<input
+					type="password"
+					value={repeatPassword}
+					onChange={(e) => validateRepeatPassword(e.target.value)}
+					onBlur={() => handleBlur()}
+				/>
+				{repeatPasswordError && (
+					<div className="error">{repeatPasswordError}</div>
+				)}
+				<button type="submit" disabled={!formValid}>
+					Зарегистрироваться
+				</button>
+			</form>
 		</div>
 	);
-};
+}
 
 export default App;
