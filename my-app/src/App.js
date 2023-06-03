c43bcad710d028a68adcb40ea0abd9b264d03804
Evgeny E <www.sam.klg@yandex.ru>
2023-06-04 00:45:18 +0300

    React Hook Form и Yup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,101 +1,53 @@
-import React, { useState, useRef } from 'react';
-import './App.css';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = yup.object().shape({
+	email: yup.string().email().required(),
+	password: yup.string().required().min(6),
+	confirmPassword: yup
+		.string()
+		.required()
+		.oneOf([yup.ref('password'), null], 'Пароли должны совпадать'),
+});
 
 function App() {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [repeatPassword, setRepeatPassword] = useState('');
-	const [emailError, setEmailError] = useState('');
-	const [passwordError, setPasswordError] = useState('');
-	const [repeatPasswordError, setRepeatPasswordError] = useState('');
-	const [formValid, setFormValid] = useState(false);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
 
-	const registerButtonRef = useRef();
-
-	const validateEmail = (email) => {
-		if (!/\S+@\S+\.\S+/.test(email)) {
-			setEmailError('Некорректный email');
-		} else {
-			setEmailError('');
-		}
-		setEmail(email);
-	};
-
-	const validatePassword = (password) => {
-		if (password.length < 6) {
-			setPasswordError('Пароль должен содержать не менее 6 символов');
-		} else {
-			setPasswordError('');
-		}
-		setPassword(password);
-	};
-
-	const validateRepeatPassword = (repeatPassword) => {
-		if (repeatPassword !== password) {
-			setRepeatPasswordError('Пароли не совпадают');
-		} else {
-			setRepeatPasswordError('');
-		}
-		setRepeatPassword(repeatPassword);
-	};
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		console.log('Email:', email);
-		console.log('Password:', password);
-		console.log('Repeat Password:', repeatPassword);
-	};
-
-	const handleBlur = () => {
-		if (
-			emailError ||
-			passwordError ||
-			repeatPasswordError ||
-			!email ||
-			!password ||
-			!repeatPassword
-		) {
-			setFormValid(false);
-		} else {
-			setFormValid(true);
-			registerButtonRef.current.focus();
-		}
+	const onSubmit = (data) => {
+		console.log(data);
 	};
 
 	return (
-		<div className="App">
-			<form onSubmit={handleSubmit}>
-				<label>Email:</label>
-				<input
-					type="email"
-					value={email}
-					onChange={(e) => validateEmail(e.target.value)}
-					onBlur={() => handleBlur()}
-				/>
-				{emailError && <div className="error">{emailError}</div>}
-				<label>Пароль:</label>
-				<input
-					type="password"
-					value={password}
-					onChange={(e) => validatePassword(e.target.value)}
-					onBlur={() => handleBlur()}
-				/>
-				{passwordError && <div className="error">{passwordError}</div>}
-				<label>Повторите пароль:</label>
-				<input
-					type="password"
-					value={repeatPassword}
-					onChange={(e) => validateRepeatPassword(e.target.value)}
-					onBlur={() => handleBlur()}
-				/>
-				{repeatPasswordError && (
-					<div className="error">{repeatPasswordError}</div>
-				)}
-				<button type="submit" disabled={!formValid} ref={registerButtonRef}>
-					Зарегистрироваться
-				</button>
-			</form>
-		</div>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<label>
+				Email:
+				<input type="text" {...register('email')} />
+				{errors.email && <div>{errors.email.message}</div>}
+			</label>
+			<label>
+				Пароль:
+				<input type="password" {...register('password')} />
+				{errors.password && <div>{errors.password.message}</div>}
+			</label>
+			<label>
+				Повторите пароль:
+				<input type="password" {...register('confirmPassword')} />
+				{errors.confirmPassword && <div>{errors.confirmPassword.message}</div>}
+			</label>
+			<button
+				type="submit"
+				disabled={!!(errors.email || errors.password || errors.confirmPassword)}
+			>
+				Зарегистрироваться
+			</button>
+		</form>
 	);
 }
 
